a54b77795ac0fdc50e134d300f844baaddc008bf
ISazhyna <seoirinas@gmail.com>
2022-02-12 20:39:00 -0600

    catch exit and cancel

--- main.py
+++ main.py
@@ -16,9 +16,17 @@ turtle.shape(image)
 data=pd.read_csv("50_states.csv")
 list_of_states=data["state"].to_list()
 
-#
 while len(list_of_states)>0:
-    answer_state = screen.textinput(title=f"{correct_quess}/50 States Correct", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{correct_quess}/50 States Correct", prompt="What's another state's name?",)
+    if answer_state:
+        answer_state.title()
+        if answer_state == "Exit":
+            pass
+        elif answer_state in list_of_states:
+            pass
+    else:
+        list_of_states=[]
+        print("exit")
 
 screen.exitonclick()
 
