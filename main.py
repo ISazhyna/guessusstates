f1fb0b9dbaa2b633085ee405cb520a285e73f923
ISazhyna <seoirinas@gmail.com>
2022-02-12 20:56:07 -0600

    count correct answers and write down state

--- main.py
+++ main.py
@@ -16,14 +16,22 @@ turtle.shape(image)
 data=pd.read_csv("50_states.csv")
 list_of_states=data["state"].to_list()
 
+#
 while len(list_of_states)>0:
     answer_state = screen.textinput(title=f"{correct_quess}/50 States Correct", prompt="What's another state's name?",)
     if answer_state:
         answer_state.title()
         if answer_state == "Exit":
-            pass
+            new_data = pd.DataFrame(list_of_states)
+            new_data.to_csv("states_to_learn.csv")
+            break
         elif answer_state in list_of_states:
-            pass
+            correct_quess += 1
+            list_of_states.remove(answer_state)
+            state_data = data[data["state"] == answer_state]
+            x_coor = int(state_data.x)
+            y_coor = int(state_data.y)
+            write_state = State(state_data.state.item(), [x_coor, y_coor])
     else:
         list_of_states=[]
         print("exit")
